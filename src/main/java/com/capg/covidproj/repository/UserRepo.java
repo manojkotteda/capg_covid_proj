fda779b2accbe67c4276d27fe7450815c9bfb9fb
manojk353 <killerkrock353@gmail.com>
2020-11-08 21:22:34 +0530

    initial commit

--- src/main/java/com/capg/covidproj/repository/UserRepo.java
+++ src/main/java/com/capg/covidproj/repository/UserRepo.java
@@ -2,9 +2,8 @@ package com.capg.covidproj.repository;
 
 import com.capg.covidproj.modal.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.stereotype.Repository;
 
-@Transactional
+@Repository
 public interface UserRepo extends JpaRepository<User, String> {
 }
