17a689544c02bb568006953307e887e5d811878c
manojk353 <killerkrock353@gmail.com>
2020-11-29 17:24:00 +0530

    revert

--- src/main/java/com/capg/covidproj/controller/Controller.java
+++ src/main/java/com/capg/covidproj/controller/Controller.java
@@ -3,12 +3,10 @@ package com.capg.covidproj.controller;
 import com.capg.covidproj.modal.User;
 import com.capg.covidproj.repository.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin()
 @RestController
 public class Controller {
 
