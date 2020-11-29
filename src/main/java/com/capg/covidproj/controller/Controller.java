c3839307acc043096dc75aefc63037114174b57c
manojk353 <killerkrock353@gmail.com>
2020-11-29 17:46:06 +0530

    crossorigin enabled

--- src/main/java/com/capg/covidproj/controller/Controller.java
+++ src/main/java/com/capg/covidproj/controller/Controller.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin
+@CrossOrigin(origins = "*",allowedHeaders = "*")
 @RestController
 public class Controller {
 
